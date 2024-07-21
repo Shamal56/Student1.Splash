5ac7271a318afc6490a30bf59648244cc4187650
Shamal56 <shamal.20230937@iit.ac.lk>
2024-07-21 10:57:37 +0530

    Add files via upload

---
+++ Splash.js
@@ -0,0 +1,6 @@
+//redirect to the home page
+function Home() {
+    window.location.href ="D:\IIT\Degree\1st Year\2nd sem\Web Design and Development\WEB CW\Student 1\VIVA\home.html";
+}
+//time for the splash screen
+setTimeout(Home, 4000);
